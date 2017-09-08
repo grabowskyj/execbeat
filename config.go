c5538a9498994947d46ee51d1910a797203b49a6
grabowskyj <89peterszabo@gmail.com>
2017-09-08 10:09:44 +0200

    Add files via upload

---
+++ config.go
@@ -0,0 +1,24 @@
+package config
+
+// Defaults for config variables which are not set
+const (
+	DefaultSchedule     string = "@every 1m"
+	DefaultDocumentType string = "execbeat"
+)
+
+type ExecbeatConfig struct {
+	Commands []ExecConfig
+}
+
+type ExecConfig struct {
+	Schedule     string
+	Command      string
+	Args         string
+	DocumentType string            `config:"document_type"`
+	Fields       map[string]string `config:"fields"`
+        Timeout      string
+}
+
+type ConfigSettings struct {
+	Execbeat ExecbeatConfig
+}
---
+++ executor.go
@@ -0,0 +1,177 @@
+package beat
+
+import (
+	"bytes"
+	"github.com/christiangalsterer/execbeat/config"
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/robfig/cron"
+	"os/exec"
+	"io"
+	"strings"
+	"syscall"
+	"time"
+)
+
+type Executor struct {
+	execbeat     *Execbeat
+	config       config.ExecConfig
+	schedule     string
+	documentType string
+	timeout      string
+}
+
+func NewExecutor(execbeat *Execbeat, config config.ExecConfig) *Executor {
+	executor := &Executor{
+		execbeat: execbeat,
+		config:   config,
+	}
+
+	return executor
+}
+
+func (e *Executor) Run() {
+
+	// setup default config
+	e.documentType = config.DefaultDocumentType
+	e.schedule = config.DefaultSchedule
+
+	// setup document type
+	if e.config.DocumentType != "" {
+		e.documentType = e.config.DocumentType
+	}
+
+	// setup cron schedule
+	if e.config.Schedule != "" {
+		logp.Debug("Execbeat", "Use schedule: [%w]", e.config.Schedule)
+		e.schedule = e.config.Schedule
+	}
+        
+        // setup command execution time
+        if e.config.Timeout != "" {
+                logp.Debug("Execbeat", "Command timeout: [%w]", e.config.Timeout)
+                e.timeout = e.config.Timeout
+        }
+
+        cron := cron.New()
+        cron.AddFunc(e.schedule, func() { e.runOneTime() })
+        cron.Start()
+
+}
+
+func getUsDuration(beginning time.Time) int64 {
+        durationSince := time.Since(beginning)
+        nsDuration := durationSince.Nanoseconds()
+        usDuration := nsDuration / 1000
+    
+        return usDuration
+}
+
+func (e *Executor) runOneTime() error {
+	var cmd *exec.Cmd
+	var cmdArgs []string
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	var waitStatus syscall.WaitStatus
+	var exitCode int = 0
+	var duration int64 = 0
+	var timeoutStr string
+	var timeoutValue time.Duration
+	var errTimeout error
+
+	cmdName := strings.TrimSpace(e.config.Command)
+
+	args := strings.TrimSpace(e.config.Args)
+	if len(args) > 0 {
+		cmdArgs = strings.Split(args, " ")
+	}
+
+        // parse timeout string to time.Duration
+        if e.timeout != "" {
+                timeoutStr = e.timeout
+                timeoutValue, errTimeout = time.ParseDuration(timeoutStr)
+
+                if errTimeout != nil {
+                             logp.Err("Execbeat", "Not valid timeout duration: %v", timeoutStr)   
+                }
+        }
+        
+	// execute command
+	now := time.Now()
+
+	if len(cmdArgs) > 0 {
+		logp.Debug("Execbeat", "Executing command: [%v] with args [%w]", cmdName, cmdArgs)
+		cmd = exec.Command(cmdName, cmdArgs...)
+	} else {
+		logp.Debug("Execbeat", "Executing command: [%v]", cmdName)
+		cmd = exec.Command(cmdName)
+	}
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+        cmd.Start()
+        
+        doneCh := make(chan error)       
+        go func() { doneCh <-cmd.Wait() }()
+
+	logp.Info("Execbeat", "Executing command: [%v]", stdout.String())
+        
+        if e.timeout != "" {
+                select {
+                case <-time.After(timeoutValue):
+                        processKillTimeStart := time.Now()
+                        cmd.Process.Kill()
+                        duration = getUsDuration(processKillTimeStart)
+                        
+                        logp.Err("Command execution has timed out after %v", timeoutStr)
+                        io.WriteString(cmd.Stderr, "Command execution has timed out")
+                        exitCode = 124
+                case errDCh := <-doneCh:
+                        duration = getUsDuration(now)
+                        
+                        if errDCh != nil {
+                                logp.Err("An error occured while executing command: %v", errDCh)
+                		exitCode = 127
+                		if exitError, ok := errDCh.(*exec.ExitError); ok {
+	                		waitStatus = exitError.Sys().(syscall.WaitStatus)
+	                		exitCode = waitStatus.ExitStatus()
+                		}       
+                        }                     
+                }
+        } else {
+                select {
+                        case errDCh := <-doneCh:
+                        duration = getUsDuration(now)
+                        
+                        if errDCh != nil {
+                                logp.Err("An error occured while executing command: %v", errDCh)
+                		exitCode = 127
+                		if exitError, ok := errDCh.(*exec.ExitError); ok {
+	                		waitStatus = exitError.Sys().(syscall.WaitStatus)
+	                		exitCode = waitStatus.ExitStatus()
+                		}       
+                        }
+                }        
+        }
+
+	commandEvent := Exec{
+		Command:  cmdName,
+		StdOut:   stdout.String(),
+		StdErr:   stderr.String(),
+		ExitCode: exitCode,
+                Duration: duration,
+	}
+
+	event := ExecEvent{
+		ReadTime:     now,
+		DocumentType: e.documentType,
+		Fields:       e.config.Fields,
+		Exec:         commandEvent,
+	}
+
+	e.execbeat.client.PublishEvent(event.ToMapStr())
+
+	return nil
+}
+
+func (e *Executor) Stop() {
+}
